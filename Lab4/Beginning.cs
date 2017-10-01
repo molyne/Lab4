2ad75bdb4be79c5fb46ad86acc3b67d44cc032ad
Molyn Eriksson <molyneriksson@gmail.com>
2017-10-01 19:49:55 +0200

    Skapade en ny klass för början av spelet som är fylld med instruktioner hur användaren ska spela.

---
+++ Lab4/Beginning.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab4
+{
+    class Beginning
+    {
+        public void Instructions()
+        {
+            Console.WriteLine("Welcome to the maze!");
+            Console.WriteLine("Your goal is to find the path to the exit with as little moves as possible.");
+            Console.WriteLine("You move yourself with W,A,S,D. A is left. D is right. W is up. S is down. ");
+            Console.WriteLine();
+            Console.WriteLine("There is obstacles and rooms you can visit when you move over them.");
+            Console.WriteLine("To move forward in the maze you have to find keys to open the door.");
+            Console.WriteLine("You can only use one key for each door. So you have to find a new key when you used one. ");
+            Console.WriteLine();
+            Console.WriteLine("k: room with a key, X: the exit , D: door, m: room with a monster, #: wall, @: you. ");
+            Console.WriteLine();
+            Console.WriteLine("Good luck! Press a key to start the game.");
+
+            Console.ReadKey();
+
+            Console.Clear();
+
+        }
+
+    }
+}
--- Lab4/Program.cs
+++ Lab4/Program.cs
@@ -5,7 +5,13 @@ namespace Lab4
     class Program
     {
         static void Main(string[] args)
-        {           
+        {
+
+            Beginning start = new Beginning();
+
+            start.Instructions();
+
+
             Map map = new Map();
 
             map.PrintMap();
